b4f345465697c5fe463fccd49eca25122992bf30
bluewhaleyt <cocohau155@gmail.com>
2024-01-10 18:26:21 +0800

    Site updated: 2024-01-10 18:26:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/react-dev-spa-notes.html"];function toRandomPost(){
+var posts=["post/react-dev-spa-notes.html","post/web-dev-build-and-host-website-notes.html","post/web-dev-express-restful-api-notes.html","post/web-dev-nodejs-express-notes.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
