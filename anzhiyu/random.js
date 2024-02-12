512942644009cc3eea81af0deb94d0b2708b0d37
bluewhaleyt <cocohau155@gmail.com>
2024-02-12 12:49:29 +0800

    Site updated: 2024-02-12 12:49:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/android-firebase-setup-notes.html","post/jetpack-compose-firebase-auth-notes.html","post/jetpack-compose-firebase-cloud-firestore-notes.html","post/jetpack-compose-firebase-realtime-database-notes.html","post/jetpack-compose-photo-picker-for-android-13.html"];function toRandomPost(){
+var posts=["post/android-firebase-setup-notes.html","post/jetpack-compose-firebase-auth-notes.html","post/jetpack-compose-firebase-realtime-database-and-cloud-firestore-notes.html","post/jetpack-compose-photo-picker-for-android-13.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
